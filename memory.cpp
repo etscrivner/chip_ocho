fb2899f742c66f2aaf62ec9b2eef1cbd407f22c3
Eric Scrivner <zenogais@gmail.com>
2009-04-30 00:25:51 -0700

    Added tests directory, my bad. Also modified memory.h to source code guidelines.

--- memory.cpp
+++ memory.cpp
@@ -1,19 +1,12 @@
 #include "memory.h"
 #include <fstream>
 
-// Function: memory::memory_t
-//
-// Clears the memory to all zeroes
 memory_t::memory_t () {
 	memset(memory, 0, sizeof(memory[0]) * CHIP_OCHO_MEMORY_SIZE);
 }
 
-// Function: memory::read
-//
-// Parameters:
-//    address - The byte address in memory
-//
-// Returns the value at the given address in memory
+///////////////////////////////////////////////////////////////////////////////
+
 const char& memory_t::read(const unsigned int& address) const {
 	if(address >= CHIP_OCHO_MEMORY_SIZE)
 		return memory[0];
@@ -21,13 +14,8 @@ const char& memory_t::read(const unsigned int& address) const {
 	return memory[address];
 }
 
-// Function: memory::write
-//
-// Parameters:
-//    address - The byte address in memory
-//    value - The value to be written
-//
-// Writes the given value at the given address in memory
+///////////////////////////////////////////////////////////////////////////////
+
 void memory_t::write(const unsigned int& address, char value) {
 	if(address >= CHIP_OCHO_MEMORY_SIZE)
 		return;
@@ -35,13 +23,8 @@ void memory_t::write(const unsigned int& address, char value) {
 	memory[address] = value;
 }
 
-// Function: memory::load
-//
-// Parameters:
-//    file_name - The name of the file to be loaded
-//
-// Loads the given file into memory at the offset CHIP_OCHO_LOAD_OFFSET.
-// Returns true if the file was loaded into memory, false otherwise.
+///////////////////////////////////////////////////////////////////////////////
+
 bool memory_t::load(const std::string& file_name) {
 	std::ifstream rom(file_name.c_str());
 	
