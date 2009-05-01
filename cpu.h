1df3fb987c9f3f1cd2a3c438381a2bb8d06a73b8
Eric Scrivner <zenogais@gmail.com>
2009-04-30 20:53:16 -0700

    Added TODO and README files.

--- cpu.h
+++ cpu.h
@@ -6,10 +6,8 @@ class cpu_t
 public:
 	cpu_t ();
 	virtual ~cpu_t ();
-
 private:
-	unsigned int pc;
-	
+	unsigned int pc; // The address of the next instruction to execute
 };
 
 #endif /* end of include guard: CPU_H_YGSGC2C5 */
